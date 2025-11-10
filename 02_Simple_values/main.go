2fa0ca9cc0ff96ae95c5ba17d6adf7d2e786b085
Jaideep3486 <jaideep.rathore@gmail.com>
2025-11-10 13:00:15 +0530

    values

---
+++ 02_Simple_values/main.go
@@ -0,0 +1,27 @@
+// Youtube Ref : https://www.youtube.com/watch?v=lf3tOqzvEgU&list=PLXQpH_kZIxTWUe-Ee-DZEX5gfeoo4tHV6&index=5
+package main
+
+import "fmt"
+
+func main() {
+
+	//this program is to go through different type of values which can be used in GoLang
+
+	//Integer
+
+	fmt.Println(1)
+
+	//Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//String
+	fmt.Println("My name is Jaideep Singh Rathore")
+
+	//Float
+	fmt.Println(1.333)
+
+	//Arithmatic operation
+	fmt.Println(7.0 / 3.0)
+
+}
